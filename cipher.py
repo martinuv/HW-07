14a1846e42adabae58692605c1c33323d3d8d178
martinuv <marth.hehe@gmail.com>
2016-01-23 23:59:56 -0600

    Initial Commit

---
+++ cipher.py
@@ -0,0 +1,19 @@
+'''cipher.py
+
+    Martin Hoffman
+    Sam Nozaki
+
+    Created 1/22/2015
+    Python Version: 3.5
+
+    Allows the user to encode text with a password via a Double Caesar 
+    (Vignere) cipher.
+
+    CS111, Winter 2016
+'''
+
+def encode(message, s, n):
+    
+
+def decode(ciphertext, s, n):
+    
